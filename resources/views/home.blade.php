aaa5726824d93f840a122ba89252a5264cf29be9
kozhen <kozhenwrya12@gmail.com>
2022-04-06 15:57:19 +0300

    first and complete commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<style>
+    ul li{
+        list-style: none;
+        display: inline;
+        background-color: whitesmoke;
+        border-radius: 5px;
+        padding: 5px;
+        margin: 3px;
+    }
+    ul{
+        background-color: tomato;
+        padding: 20px;
+        border-radius: 5px;
+    }
+    a{
+        color: black;
+        text-decoration: none;
+    }
+</style>
+<ul>
+    <li><a href="profile">Profile</a></li>
+    <li><a href="profile">Biography</a></li>
+
+</ul>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <style>
+    * {
+	box-sizing: border-box;
+}
+
+body {
+	background-color: #28223F;
+	font-family: Montserrat, sans-serif;
+	
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+
+	min-height: 100vh;
+	margin: 0;
+}
+.pro {
+	background-color: #231E39;
+	border-radius: 5px;
+	box-shadow: 0px 10px 20px -10px rgba(0,0,0,0.75);
+	color: #B3B8CD;
+	padding-top: 30px;
+	position: relative;
+	width: 350px;
+	max-width: 100%;
+	text-align: center;
+}
+.pro .round {
+	border: 1px solid #03BFCB;
+	border-radius: 50%;
+	padding: 7px;
+    height: 150px;
+    width: 150px;
+}
+h3{
+    text-align: left;
+}
+ul li{
+    list-style: none;
+}
+
+   </style>
+</head>
+<body>
+    
+</body>
+</html>
+<div class="pro">
+<img class="round" src="rasm/image.jpg"  />
+<ul>
+    <li><h3>Name: Kozhen Wrya Mahmud</h3></li>
+    <li><h3>Age: 22</h3></li>
+    <li><h3>Department: IT</h3></li>
+    <li><h3>Stage: third</h3></li>
+    <li><h3>Email: kozhenwrya12@gmail.com</h3></li>
+</ul>
+
+</div>
+
+
+
+
